097dcf1a2c7a775d4e342f47b83b8ce328c391a7
Musa Jahun <msjahun@live.com>
2018-06-27 02:32:06 +0300

    Connect the controller with the View, Localization and Internationalization now works

--- Localization_and_internalization_C_sharp/Controllers/LocaleController.cs
+++ Localization_and_internalization_C_sharp/Controllers/LocaleController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +14,36 @@ namespace Localization_and_internalization_C_sharp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Change(string id)
+        {
+            CultureInfo ci = new CultureInfo(id);
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+            //Number Format
+
+            int n = 12345678;
+            ViewBag.NumberFormat = n.ToString("N", Thread.CurrentThread.CurrentCulture);
+
+            //Percent Format
+            double percent = 12.34;
+            ViewBag.PercentFormat = percent.ToString("P", Thread.CurrentThread.CurrentCulture);
+
+            //Currency Format
+            int currency = 123456;
+            ViewBag.CurrencyFormat = currency.ToString("C", Thread.CurrentThread.CurrentCulture);
+
+
+            //Date Time Format
+            ViewBag.DateTimeFormat = DateTime.Now.ToString("D", Thread.CurrentThread.CurrentCulture);
+
+            //Content Format
+            ViewBag.ContentFormat = Resources.Content.Message;
+
+            return View("Index");
+
+            
         }
     }
 }
