3219e9bb6611066103298d18533256fb91e70476
Musa Jahun <msjahun@live.com>
2018-06-27 01:53:46 +0300

    Created the LocaleController

---
+++ Localization_and_internalization_C_sharp/Controllers/LocaleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Localization_and_internalization_C_sharp.Controllers
+{
+    public class LocaleController : Controller
+    {
+        // GET: Locale
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
