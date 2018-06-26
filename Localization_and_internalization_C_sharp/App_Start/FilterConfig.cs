1c124bb47f4d3fb609cd17eb52add683fe6b8b66
Musa Jahun <msjahun@live.com>
2018-06-27 01:37:43 +0300

    Add project files.

---
+++ Localization_and_internalization_C_sharp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Localization_and_internalization_C_sharp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Localization_and_internalization_C_sharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Localization_and_internalization_C_sharp.MvcApplication" Language="C#" %>
